e5c83571ca5bbf374c9e31c359af907254ca6b63
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-04-24 15:11:03 +0530

    Starting to learn SignIn & SighUp

---
+++ app/src/main/java/com/example/aa10learningapp/Signup_login.kt
@@ -0,0 +1,11 @@
+package com.example.aa10learningapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_login)
+    }
+}
